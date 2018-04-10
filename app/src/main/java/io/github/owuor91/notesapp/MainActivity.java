17f49057f09f0ae64270db7dfe19cf3dc09a227e
John Owuor <jo@refunite.org>
2018-04-10 17:26:29 +0300

    add add note activity

--- app/src/main/java/io/github/owuor91/notesapp/MainActivity.java
+++ app/src/main/java/io/github/owuor91/notesapp/MainActivity.java
@@ -1,8 +1,8 @@
 package io.github.owuor91.notesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,13 +12,14 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    FloatingActionButton fab = findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        startActivity(new Intent(getBaseContext(), AddNoteActivity.class));
       }
     });
   }
