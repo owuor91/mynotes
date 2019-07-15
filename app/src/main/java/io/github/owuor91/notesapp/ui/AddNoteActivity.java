d7319a3f3f235a6632202a1cd120db8c3205552c
John Owuor <owuor91@gmail.com>
2019-07-15 12:46:51 +0300

    updates

--- app/src/main/java/io/github/owuor91/notesapp/ui/AddNoteActivity.java
+++ app/src/main/java/io/github/owuor91/notesapp/ui/AddNoteActivity.java
@@ -1,17 +1,22 @@
 package io.github.owuor91.notesapp.ui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import io.github.owuor91.notesapp.R;
 
 public class AddNoteActivity extends AppCompatActivity {
   static final int REQUEST_IMAGE_CAPTURE = 1;
   TextView tvAddVoiceNote, tvAddPhoto;
+  Button btnSaveNote;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -21,9 +26,10 @@ public class AddNoteActivity extends AppCompatActivity {
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-    tvAddPhoto = (TextView) findViewById(R.id.tvAddPhoto);
+    tvAddPhoto = findViewById(R.id.tvAddPhoto);
+    btnSaveNote = findViewById(R.id.btnSaveNote);
 
-    tvAddVoiceNote = (TextView) findViewById(R.id.tvAddVoiceNote);
+    tvAddVoiceNote = findViewById(R.id.tvAddVoiceNote);
     tvAddVoiceNote.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         startActivity(new Intent(getBaseContext(), RecordAudioActivity.class));
@@ -38,5 +44,15 @@ public class AddNoteActivity extends AppCompatActivity {
         }
       }
     });
+  }
+
+  @Override protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+      Bundle extras = data.getExtras();
+
+      Bitmap bitmap = (Bitmap) extras.get("data");
+      Log.d("TAG", bitmap.toString());
+    }
   }
 }
